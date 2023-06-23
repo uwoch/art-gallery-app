71cf3e699b4604c84dcc596c6a48e712c9694be1
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-06-23 09:25:28 +0200

    add userStory1

---
+++ components/ArtPiecePreview/index.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function ArtPiecePreview({ name, artist, imageSource }) {
+  return (
+    <section>
+      <>
+        <h3>{name}</h3>
+        <p>{artist}</p>
+        <Image
+          src={imageSource}
+          alt="Art piece image"
+          width={200}
+          height={200}
+        />
+      </>
+    </section>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "../styles";
 
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps}
+       />
     </>
   );
 }
